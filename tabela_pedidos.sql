7121e5515e091987dff96852bfe8ab4b44fdeb57
Filipe Astolfi <72992937+FilipeAstolfi@users.noreply.github.com>
2020-10-29 16:03:31 -0300

    Add files via upload

---
+++ tabela_pedidos.sql
@@ -0,0 +1,13 @@
+/*
+-- Query: SELECT * FROM fseletro.pedidos
+LIMIT 0, 1000
+
+-- Date: 2020-10-29 15:59
+*/
+INSERT INTO `` (`idpedidos`,`nome_cliente`,`endereco`,`produto`,`valor_unitario`,`quantidade`,`valortotal`) VALUES (NULL,'Gilberto Astolfi','Rua Guatemala, 305 Campo Limpo Paulista - SP','Micro-ondas 20L Electrolux MTO30',619.00,1,619.00);
+INSERT INTO `` (`idpedidos`,`nome_cliente`,`endereco`,`produto`,`valor_unitario`,`quantidade`,`valortotal`) VALUES (NULL,'Valter de Lima','Rua Regueira Campo, 454  Recife - PE','Lavadora de Roupas LG Smart 11Kg 14 Programas de Lavagem',1499.00,2,2998.00);
+INSERT INTO `` (`idpedidos`,`nome_cliente`,`endereco`,`produto`,`valor_unitario`,`quantidade`,`valortotal`) VALUES (NULL,'Amália Bispo dos Santos','Rua Prof. Antônio Seixas Leite Ribeiro Acesso 16 Bloco 114 Apto 41 Santo André - SP','Micro-ondas 38l brastemp ative inox',1719.00,1,1719.00);
+INSERT INTO `` (`idpedidos`,`nome_cliente`,`endereco`,`produto`,`valor_unitario`,`quantidade`,`valortotal`) VALUES (NULL,'Elietes dos Santos','Rua Guatemala, 305 Campo Limpo Paulista - SP','Lava-Louças 10 Serviços Inox Electrolux - 127V',2299.00,1,2299.00);
+INSERT INTO `` (`idpedidos`,`nome_cliente`,`endereco`,`produto`,`valor_unitario`,`quantidade`,`valortotal`) VALUES (NULL,'Filipe Astolfi','Rua Cotoxó, 1175 São Paulo - SP','Geladeira Brastemp Frost Free 375L',5019.00,1,5019.00);
+INSERT INTO `` (`idpedidos`,`nome_cliente`,`endereco`,`produto`,`valor_unitario`,`quantidade`,`valortotal`) VALUES (NULL,'Isabella Alves','Rua Dr. Miranda de Azevedo, 786 São Paulo - SP','Fogão 4 Bocas Consul Acendimento Automático',1699.00,1,1699.00);
+INSERT INTO `` (`idpedidos`,`nome_cliente`,`endereco`,`produto`,`valor_unitario`,`quantidade`,`valortotal`) VALUES (NULL,'Lellye Lima','Rua Cotoxó, 1175','Lava-Louças 10 Serviços Inox Electrolux - 127V',2299.00,1,NULL);
---
+++ tabela_produtos.sql
@@ -0,0 +1,18 @@
+/*
+-- Query: SELECT * FROM fseletro.produto
+LIMIT 0, 1000
+
+-- Date: 2020-10-29 16:00
+*/
+INSERT INTO `` (`idproduto`,`categoria`,`descricao`,`preco`,`preco_final`,`foto`) VALUES (1,'geladeira','Geladeira Brastemp Frost Free 375L',6389.00,5019.00,'Imagens/geladeira.jpeg');
+INSERT INTO `` (`idproduto`,`categoria`,`descricao`,`preco`,`preco_final`,`foto`) VALUES (2,'geladeira','Refrigerador Frost Free Brastemp Side Inverse 540 L',4561.00,2499.00,'Imagens/geladeira2.jpeg');
+INSERT INTO `` (`idproduto`,`categoria`,`descricao`,`preco`,`preco_final`,`foto`) VALUES (3,'geladeira','Refrigerador Consul Frost Free Evox - Duplex 386L',5189.00,3599.00,'Imagens/geladeira3.jpeg');
+INSERT INTO `` (`idproduto`,`categoria`,`descricao`,`preco`,`preco_final`,`foto`) VALUES (4,'fogao','Fogão 4 Bcs Fratello Automático MuellerBranco',1999.00,1019.00,'Imagens/fog%C3%A3o.jpeg');
+INSERT INTO `` (`idproduto`,`categoria`,`descricao`,`preco`,`preco_final`,`foto`) VALUES (5,'fogao','Fogão 4 Bocas Consul Acendimento Automático',2299.00,1699.00,'Imagens/fogao2.jpeg');
+INSERT INTO `` (`idproduto`,`categoria`,`descricao`,`preco`,`preco_final`,`foto`) VALUES (6,'microondas','Micro-ondas 20L Electrolux MTO30',899.00,619.00,'Imagens/microondas.jpeg');
+INSERT INTO `` (`idproduto`,`categoria`,`descricao`,`preco`,`preco_final`,`foto`) VALUES (7,'microondas','Micro-ondas 38l brastemp ative inox',2050.00,1719.00,'Imagens/microondas2.jpeg');
+INSERT INTO `` (`idproduto`,`categoria`,`descricao`,`preco`,`preco_final`,`foto`) VALUES (8,'microondas','Microondas Midea 31L Branco Espelhado MTFE 220V',800.00,549.00,'Imagens/microondas3.jpeg');
+INSERT INTO `` (`idproduto`,`categoria`,`descricao`,`preco`,`preco_final`,`foto`) VALUES (9,'lava-roupas','Lavadora de Roupas 9kg Brastemp - Ciclo Tira Manchas',1699.00,1099.00,'Imagens/lavadoura.jpeg');
+INSERT INTO `` (`idproduto`,`categoria`,`descricao`,`preco`,`preco_final`,`foto`) VALUES (10,'lava-roupas','Lavadora de Roupas LG Smart 11Kg 14 Programas de Lavagem',2199.00,1499.00,'Imagens/lavadoura2.jpeg');
+INSERT INTO `` (`idproduto`,`categoria`,`descricao`,`preco`,`preco_final`,`foto`) VALUES (11,'lava-louca','Lava-Louças 8 Serviços Branco Electrolux (LL08B)',4500.00,3500.00,'Imagens/lavalouca.jpeg');
+INSERT INTO `` (`idproduto`,`categoria`,`descricao`,`preco`,`preco_final`,`foto`) VALUES (12,'lava-louca','Lava-Louças 10 Serviços Inox Electrolux - 127V',3500.00,2299.00,'Imagens/lavalouca2.jpeg');
