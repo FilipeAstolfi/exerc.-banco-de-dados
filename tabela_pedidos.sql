6807e682bdda47a071f67eef7a3f32cc0de6dd91
Filipe Astolfi <72992937+FilipeAstolfi@users.noreply.github.com>
2020-10-29 16:16:08 -0300

    Add files via upload

--- tabela_pedidos.sql
+++ tabela_pedidos.sql
@@ -2,7 +2,7 @@
 -- Query: SELECT * FROM fseletro.pedidos
 LIMIT 0, 1000
 
--- Date: 2020-10-29 15:59
+-- Date: 2020-10-29 16:13
 */
 INSERT INTO `` (`idpedidos`,`nome_cliente`,`endereco`,`produto`,`valor_unitario`,`quantidade`,`valortotal`) VALUES (NULL,'Gilberto Astolfi','Rua Guatemala, 305 Campo Limpo Paulista - SP','Micro-ondas 20L Electrolux MTO30',619.00,1,619.00);
 INSERT INTO `` (`idpedidos`,`nome_cliente`,`endereco`,`produto`,`valor_unitario`,`quantidade`,`valortotal`) VALUES (NULL,'Valter de Lima','Rua Regueira Campo, 454  Recife - PE','Lavadora de Roupas LG Smart 11Kg 14 Programas de Lavagem',1499.00,2,2998.00);
@@ -10,4 +10,7 @@ INSERT INTO `` (`idpedidos`,`nome_cliente`,`endereco`,`produto`,`valor_unitario`
 INSERT INTO `` (`idpedidos`,`nome_cliente`,`endereco`,`produto`,`valor_unitario`,`quantidade`,`valortotal`) VALUES (NULL,'Elietes dos Santos','Rua Guatemala, 305 Campo Limpo Paulista - SP','Lava-Louças 10 Serviços Inox Electrolux - 127V',2299.00,1,2299.00);
 INSERT INTO `` (`idpedidos`,`nome_cliente`,`endereco`,`produto`,`valor_unitario`,`quantidade`,`valortotal`) VALUES (NULL,'Filipe Astolfi','Rua Cotoxó, 1175 São Paulo - SP','Geladeira Brastemp Frost Free 375L',5019.00,1,5019.00);
 INSERT INTO `` (`idpedidos`,`nome_cliente`,`endereco`,`produto`,`valor_unitario`,`quantidade`,`valortotal`) VALUES (NULL,'Isabella Alves','Rua Dr. Miranda de Azevedo, 786 São Paulo - SP','Fogão 4 Bocas Consul Acendimento Automático',1699.00,1,1699.00);
-INSERT INTO `` (`idpedidos`,`nome_cliente`,`endereco`,`produto`,`valor_unitario`,`quantidade`,`valortotal`) VALUES (NULL,'Lellye Lima','Rua Cotoxó, 1175','Lava-Louças 10 Serviços Inox Electrolux - 127V',2299.00,1,NULL);
+INSERT INTO `` (`idpedidos`,`nome_cliente`,`endereco`,`produto`,`valor_unitario`,`quantidade`,`valortotal`) VALUES (NULL,'Lellye Lima','Rua Cotoxó, 1175','Lava-Louças 10 Serviços Inox Electrolux - 127V',2299.00,1,2299.00);
+INSERT INTO `` (`idpedidos`,`nome_cliente`,`endereco`,`produto`,`valor_unitario`,`quantidade`,`valortotal`) VALUES (NULL,'Renan Manoel','Rua Okinawa, 180 Santo André - SP','Micro-ondas 20L Electrolux MTO30',619.00,2,1238.00);
+INSERT INTO `` (`idpedidos`,`nome_cliente`,`endereco`,`produto`,`valor_unitario`,`quantidade`,`valortotal`) VALUES (NULL,'Fátima Xavier','Avenida  Norte, 3874 Recife - PE','Lava-Louças 10 Serviços Inox Electrolux - 127V',2299.00,2,4598.00);
+INSERT INTO `` (`idpedidos`,`nome_cliente`,`endereco`,`produto`,`valor_unitario`,`quantidade`,`valortotal`) VALUES (NULL,'Matheus Flores','Rua Zacarias de Góes, 376 Jundiaí - SP','Micro-ondas 20L Electrolux MTO30',619.00,1,619.00);
